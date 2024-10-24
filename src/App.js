ca1a9696f5ca1506782760aa50c983a3c0f6e505
Mohamed Amin Amri <amrim893@gmail.com>
2024-10-24 17:10:59 +0200

    pokemonapi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  const fetchPokemon = async () => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=807');
+    const data = await response.json();
+    const pokemonNames = data.results.map((pokemon) => pokemon.name);
+    setPokemonList(pokemonNames);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <button onClick={fetchPokemon}>Fetch Pokemon</button>
+      <ul>
+        {pokemonList.map((pokemon, index) => (
+          <li key={index}>{pokemon}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
